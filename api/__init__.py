b3d869ccb0553208f6f58976a085a6a3b530cd0c
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-03 19:41:06 +0500

    include users router

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from .products import products_router
-
+from .users import users_router
 
 router = APIRouter(
     prefix='/api/v1'
 )
 
 router.include_router(products_router)
+router.include_router(users_router)
