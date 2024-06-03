8c224eb62b5bf651bc936393002b15cd96357ee7
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-03 19:19:51 +0500

    add relationship for profile model

--- core/models/users.py
+++ core/models/users.py
@@ -3,9 +3,11 @@ from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .posts import Post
+    from .profiles import Profile
 
 
 class User(Base):
@@ -14,3 +16,4 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(30), unique=True)
 
     posts: Mapped[list['Post']] = relationship(back_populates='user')
+    profile: Mapped['Profile'] = relationship(back_populates='user')
