d6e75fe736ded3b11af3796961f12cf802362b67
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-03 18:41:56 +0500

    add __all__ method for export models

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,11 @@
+__all__ = (
+    'Base',
+    'db_helper',
+    'Product',
+    'User'
+)
+
 from .base import Base
 from .db_helper import db_helper
 from .products import Product
+from .users import User
