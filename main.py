be79d7b80e192a88f7219d10dc7aee413a454146
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-03 18:34:17 +0500

    delete lifespan

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from core.models import Base, db_helper
 from api import router as apiv1_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
 
 
