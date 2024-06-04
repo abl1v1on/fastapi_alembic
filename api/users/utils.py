7ac4bb766f3c6eecfdd9fb91a7ae8396363cb6d8
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-04 12:53:54 +0500

    create get all users and get user func

--- api/users/utils.py
+++ api/users/utils.py
@@ -1,7 +1,20 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import UserCreate
 from core.models import User, Profile
+
+
+async def get_all_users(session: AsyncSession) -> list[User]:
+    query = select(User)
+    res = await session.execute(query)
+    return res.scalars().all()
+
+
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
+    query = select(User).where(User.id == user_id)
+    res = await session.execute(query)
+    return res.scalar()
 
 
 async def create_user(session: AsyncSession, user: UserCreate) -> User:
@@ -9,4 +22,3 @@ async def create_user(session: AsyncSession, user: UserCreate) -> User:
     session.add(user)
     await session.commit()
     return user
-
