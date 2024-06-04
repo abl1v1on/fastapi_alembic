6b64b5c6c50336e3507d403bb649c42f0ab80491
Maxim Danilov <abl1v1on@yandex.ru>
2024-06-04 12:54:16 +0500

    created endpoints for get user list and get user by id

--- api/users/views.py
+++ api/users/views.py
@@ -1,5 +1,6 @@
+from pydantic import PositiveInt
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Depends
 
 from . import utils
 from .schemas import UserCreate, User
@@ -11,7 +12,27 @@ router = APIRouter(
     tags=['Users']
 )
 
+SessionDependency = Depends(db_helper.session_dependency)
+
+
+@router.get('/', response_model=list[User])
+async def get_users(session: AsyncSession = SessionDependency):
+    return await utils.get_all_users(session)
+
+
+@router.get('/{user_id}')
+async def get_user_by_id(user_id: PositiveInt, session: AsyncSession = SessionDependency):
+    user = await utils.get_user(session, user_id)
+    
+    if not user:
+        raise HTTPException(
+            detail='User not found',
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    
+    return user
+
 
 @router.post('/', response_model=User)
-async def create_user(user: UserCreate, session: AsyncSession = Depends(db_helper.session_dependency)):
+async def create_user(user: UserCreate, session: AsyncSession = SessionDependency):
     return await utils.create_user(session, user)
